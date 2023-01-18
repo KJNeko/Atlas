6d2ca35d072b45a02a029f30d311c2cf686bbb88
kj16609 <bigskyman02@gmail.com>
2023-01-18 17:05:07 -0500

    Prototype record view backend

---
+++ src/h95/backend/RecordViewBackend.cpp
@@ -0,0 +1,8 @@
+//
+// Created by kj16609 on 1/18/23.
+//
+
+#include "RecordViewBackend.hpp"
+
+
+
---
+++ src/h95/backend/RecordViewBackend.hpp
@@ -0,0 +1,37 @@
+//
+// Created by kj16609 on 1/18/23.
+//
+
+#ifndef HYDRUS95_RECORDVIEWBACKEND_HPP
+#define HYDRUS95_RECORDVIEWBACKEND_HPP
+
+#include <QObject>
+#include "h95/database/Tag.hpp"
+
+class RecordViewBackend : public QObject
+{
+	Q_OBJECT
+
+	std::vector<Tag> active_tags {};
+
+	public slots:
+	//! Starts a search for a similar tag to the text.
+	void searchSimilar(QString text);
+	//! Adds a tag to the taglist
+	void addTag(const Tag& tag);
+	//! Removes a tag from the taglist
+	void removeTag(const Tag& tag);
+
+	signals:
+	//! Emitted when a search for similar tags is done
+	void similarTagFinished(const std::vector<Tag>& tags);
+
+	//! Emitted when the tag list has changed
+	void tagsChanged(const std::vector<Tag>& tags);
+
+	//! Emitted when a search is completed for tags or when a new record is added
+	void recordsUpdated(const std::vector<Record>& records);
+};
+
+
+#endif	//HYDRUS95_RECORDVIEWBACKEND_HPP
