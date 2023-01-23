91ba7c0d0ac17397b085602798f927478f7fee89
kj16609 <bigskyman02@gmail.com>
2023-01-22 23:01:36 -0500

    Change query to use an enum instead of a string for identifying preview vs banner

--- src/h95/database/Record.cpp
+++ src/h95/database/Record.cpp
@@ -15,6 +15,13 @@ bool Record::operator==( const Record& other ) const
 }
 #endif
 
+enum PreviewType
+{
+	PREVIEW_UNKNOWN = 0,
+	PREVIEW_BANNER,
+	PREVIEW_PREVIEW
+};
+
 Record Record::create(
 	const QString& title,
 	const QString& creator,
@@ -26,20 +33,24 @@ Record Record::create(
 {
 	ZoneScoped;
 	RecordID id { 0 };
-	database::db_ref()
-			<< "INSERT INTO records (title, creator, version, engine) VALUES (?, ?, ?, ?) RETURNING record_id"
-			<< title.toStdString() << creator.toStdString() << version.toStdString() << engine.toStdString()
-		>> [&]( const RecordID record_id )
-	{
-		id = record_id;
-	};
 
-	std::vector< std::pair< std::string, std::string > > image_paths;
-	if ( !banner.empty() ) image_paths.emplace_back( banner.string(), "banner" );
-	for ( const auto& preview : previews ) image_paths.emplace_back( preview.string(), "preview" );
+	{
+		ZoneScopedN( "Query" );
+		database::db_ref()
+				<< "INSERT INTO records (title, creator, version, engine) VALUES (?, ?, ?, ?) RETURNING record_id"
+				<< title.toStdString() << creator.toStdString() << version.toStdString() << engine.toStdString()
+			>> [&]( const RecordID record_id )
+		{
+			id = record_id;
+		};
+	}
+
+	std::vector< std::pair< std::string, PreviewType > > image_paths;
+	if ( !banner.empty() ) image_paths.emplace_back( banner.string(), PREVIEW_BANNER );
+	for ( const auto& preview : previews ) image_paths.emplace_back( preview.string(), PREVIEW_PREVIEW);
 
 	for ( const auto& [path, type] : image_paths )
-		database::db_ref() << "INSERT INTO previews (record_id, type, path) VALUES (?, ?, ?)" << id << type << path;
+		database::db_ref() << "INSERT INTO previews (record_id, type, path) VALUES (?, ?, ?)" << id << static_cast<uint8_t>(type) << path;
 
 	return { id, title, creator, version, engine, GameMetadata::insert( id, metadata ), banner, previews };
 }
@@ -52,29 +63,46 @@ Record Record::select( const RecordID id )
 	QString version;
 	QString engine;
 
-	database::db_ref() << "SELECT title, creator, version, engine FROM records WHERE record_id = ?" << id >>
-		[&]( const std::string title_in,
-			 const std::string creator_in,
-			 const std::string version_in,
-			 const std::string engine_in )
 	{
-		title = QString::fromStdString( title_in );
-		creator = QString::fromStdString( creator_in );
-		version = QString::fromStdString( version_in );
-		engine = QString::fromStdString( engine_in );
-	};
+		ZoneScopedN( "Query Inital" );
+		database::db_ref() << "SELECT title, creator, version, engine FROM records WHERE record_id = ?" << id >>
+			[&]( const std::string& title_in,
+				 const std::string& creator_in,
+				 const std::string& version_in,
+				 const std::string& engine_in )
+		{
+			title = QString::fromStdString( title_in );
+			creator = QString::fromStdString( creator_in );
+			version = QString::fromStdString( version_in );
+			engine = QString::fromStdString( engine_in );
+		};
+	}
+
 
 	std::filesystem::path banner_path { ":invalid_banner.jpg" };
 	std::vector< std::filesystem::path > preview_paths;
+	preview_paths.reserve(32);
 
-	database::db_ref() << "SELECT path, type FROM previews WHERE record_id = ?" << id >>
-		[&]( const std::string path, const std::string type )
 	{
-		if ( type == "banner" )
-			banner_path = path;
-		else
-			preview_paths.emplace_back( path );
-	};
+		ZoneScopedN( "Query previews" );
+		database::db_ref() << "SELECT path, type FROM previews WHERE record_id = ?" << id >>
+			[&banner_path, &preview_paths]( std::string&& path, const uint8_t type )
+		{
+			ZoneScopedN("Process row");
+			switch(static_cast<PreviewType>(type))
+			{
+				default: [[fallthrough]];
+				case PREVIEW_UNKNOWN:
+					break;
+				case PREVIEW_BANNER:
+					banner_path = std::move(path);
+					break;
+				case PREVIEW_PREVIEW:
+					preview_paths.emplace_back(std::move(path));
+					break;
+			}
+		};
+	}
 
 	return { id, title, creator, version, engine, GameMetadata::select( id ), banner_path, preview_paths };
 }
