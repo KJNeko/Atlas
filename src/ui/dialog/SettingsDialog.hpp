485d0d3b705fa34d862960d071489031d6d5e47e
kj16609 <bigskyman02@gmail.com>
2023-01-23 07:20:49 -0500

    Add SettingsDialog

---
+++ src/ui/dialog/SettingsDialog.hpp
@@ -0,0 +1,31 @@
+//
+// Created by kj16609 on 1/23/23.
+//
+
+#ifndef HYDRUS95_SETTINGSDIALOG_HPP
+#define HYDRUS95_SETTINGSDIALOG_HPP
+
+#include <QDialog>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+	class SettingsDialog;
+}
+QT_END_NAMESPACE
+
+class SettingsDialog : public QDialog
+{
+	Q_OBJECT
+
+	public:
+	explicit SettingsDialog( QWidget* parent = nullptr );
+	~SettingsDialog() override;
+
+	private:
+	Ui::SettingsDialog* ui;
+};
+
+
+#endif	//HYDRUS95_SETTINGSDIALOG_HPP
