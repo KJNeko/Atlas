ee7769ef873b44cca88b8b1081f8e388fc8709dd
kj16609 <bigskyman02@gmail.com>
2023-01-22 23:00:20 -0500

    Tracy memory profiling

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,52 @@
 #include "h95/database/database.hpp"
 
 #include "ui/MainWindow.hpp"
+#include <tracy/Tracy.hpp>
+
+#ifdef TRACY_ENABLE
+void* operator new( std::size_t count )
+{
+	auto ptr = malloc( count );
+	TracySecureAlloc( ptr, count );
+	return ptr;
+}
+
+
+void* operator new[]( std::size_t count )
+{
+	auto ptr = malloc( count );
+	TracySecureAlloc( ptr, count );
+	return ptr;
+}
+
+
+void operator delete( void* ptr ) noexcept
+{
+	TracySecureFree( ptr );
+	free( ptr );
+}
+
+
+void operator delete[]( void* ptr ) noexcept
+{
+	TracySecureFree( ptr );
+	free( ptr );
+}
+
+
+void operator delete( void* ptr, std::size_t ) noexcept
+{
+	TracySecureFree( ptr );
+	free( ptr );
+}
+
+
+void operator delete[]( void* ptr, std::size_t ) noexcept
+{
+	TracySecureFree( ptr );
+	free( ptr );
+}
+#endif
 
 int main( int argc, char** argv )
 {
