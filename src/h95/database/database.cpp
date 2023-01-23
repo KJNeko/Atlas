abcf2b990ea1b691e282135539f1e0bd6490c4a8
kj16609 <bigskyman02@gmail.com>
2023-01-22 23:00:13 -0500

    Add primary key constraint to record id for other tables to induce index

--- src/h95/database/database.cpp
+++ src/h95/database/database.cpp
@@ -29,9 +29,9 @@ namespace database
 
 		const std::vector< std::string > table_strs {
 			"CREATE TABLE IF NOT EXISTS records (record_id INTEGER PRIMARY KEY, title TEXT, creator TEXT, version TEXT, engine TEXT, unique(title, creator, version, engine))",
-			"CREATE TABLE IF NOT EXISTS game_metadata (record_id INTEGER REFERENCES records(record_id), game_path TEXT, exec_path TEXT)",
-			"CREATE TABLE IF NOT EXISTS previews (record_id INTEGER REFERENCES records(record_id), type TEXT, path TEXT)",
-			"CREATE TABLE IF NOT EXISTS flags (record_id INTEGER REFERENCES records(record_id), installed INTEGER, played INTEGER, wanted INTEGER)" };
+			"CREATE TABLE IF NOT EXISTS game_metadata (record_id INTEGER REFERENCES records(record_id) PRIMARY KEY, game_path TEXT, exec_path TEXT)",
+			"CREATE TABLE IF NOT EXISTS previews (record_id INTEGER REFERENCES records(record_id) PRIMARY KEY, type TEXT, path TEXT)",
+			"CREATE TABLE IF NOT EXISTS flags (record_id INTEGER REFERENCES records(record_id) PRIMARY KEY, installed INTEGER, played INTEGER, wanted INTEGER)" };
 
 		for ( const auto& query_str : table_strs ) { db_ref() << query_str; }
 	}
