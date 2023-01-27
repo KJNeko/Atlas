47307d4c31a2a38d994504cd78da305afbf54a6a
kj16609 <bigskyman02@gmail.com>
2023-01-27 11:00:40 -0500

    Remove unneeded initalization of db in main

--- src/main.cpp
+++ src/main.cpp
@@ -71,10 +71,6 @@ int main( int argc, char** argv )
 
 	QApplication app { argc, argv };
 
-	database::initalize();
-
-	//Do basic setup to ensure all paths are proper
-
 	const auto path	{getSettings< QString >( "paths/data", "./data/games" ).toStdString()};
 	std::filesystem::create_directories(path);
 
