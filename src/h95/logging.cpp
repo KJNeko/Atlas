73b611ee442263b0902a564968bdbab11770d16a
kj16609 <bigskyman02@gmail.com>
2023-01-30 14:12:27 -0500

    Add debug define for logger

--- src/h95/logging.cpp
+++ src/h95/logging.cpp
@@ -15,7 +15,13 @@ void initLogging()
 	spdlog::enable_backtrace( 32 );
 
 	auto console_sink { std::make_shared< spdlog::sinks::stdout_color_sink_mt >() };
+
+#ifndef NDEBUG
+	console_sink->set_level( spdlog::level::debug );
+#else
 	console_sink->set_level( spdlog::level::info );
+#endif
+
 
 	auto file_sink {
 		std::make_shared< spdlog::sinks::rotating_file_sink_mt >( "./data/logs/log.txt", 1024 * 1024 * 1, 3 ) };
@@ -25,7 +31,7 @@ void initLogging()
 
 	auto logger { std::make_shared< spdlog::logger >( "", sinks.begin(), sinks.end() ) };
 	logger->set_level( spdlog::level::debug );
-	logger->info("Logger setup");
+	logger->info( "Logger setup" );
 
 	spdlog::set_default_logger( logger );
 
