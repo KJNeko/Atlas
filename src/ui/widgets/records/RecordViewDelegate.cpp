5535c1d8025705924f3e954f00897ea3d86f3aae
kj16609 <bigskyman02@gmail.com>
2023-02-02 00:37:31 -0500

    Formatting

--- src/ui/widgets/records/RecordViewDelegate.cpp
+++ src/ui/widgets/records/RecordViewDelegate.cpp
@@ -27,15 +27,18 @@ void RecordViewDelegate::paint( QPainter* painter, const QStyleOptionViewItem& o
 		const auto banner_width { getSettings< int >( "main_view/banner_width", 400 ) };
 		const auto banner_height { getSettings< int >( "main_view/banner_height", 300 ) };
 
-		const auto banner_path { QString::fromStdString((record.m_metadata.game_path / record.m_banner).string())};
+		const auto banner_path { QString::fromStdString( ( record.m_metadata.game_path / record.m_banner ).string() ) };
 
-		QPixmap banner {":/invalid_banner.jpg"};
-		if(!QPixmapCache::find( banner_path, &banner) && std::filesystem::exists(record.m_metadata.game_path / record.m_banner))
+		QPixmap banner { ":/invalid_banner.jpg" };
+		if ( !QPixmapCache::find( banner_path, &banner )
+			 && std::filesystem::exists( record.m_metadata.game_path / record.m_banner ) )
 		{
-			banner = QPixmap(banner_path);banner = banner.scaledToHeight( banner_height, Qt::SmoothTransformation );
-			if ( banner.width() > banner_width ) banner = banner.scaledToWidth( banner_width, Qt::SmoothTransformation );
+			banner = QPixmap( banner_path );
+			banner = banner.scaledToHeight( banner_height, Qt::SmoothTransformation );
+			if ( banner.width() > banner_width )
+				banner = banner.scaledToWidth( banner_width, Qt::SmoothTransformation );
 
-			QPixmapCache::insert(banner_path, banner);
+			QPixmapCache::insert( banner_path, banner );
 		}
 
 		const QRect pixmap_rect { option.rect.center(), QSize( banner.width(), banner.height() ) };
@@ -44,12 +47,12 @@ void RecordViewDelegate::paint( QPainter* painter, const QStyleOptionViewItem& o
 			banner );
 
 
-		if(option.state & QStyle::State_Selected)
+		if ( option.state & QStyle::State_Selected )
 		{
 			painter->save();
 
-			painter->setBrush(QColor(0,0,255, 50));
-			painter->drawRect(option.rect);
+			painter->setBrush( QColor( 0, 0, 255, 50 ) );
+			painter->drawRect( option.rect );
 
 			painter->restore();
 		}
