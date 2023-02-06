34a5b45c43b924656b2c12a748af3695ad93b371
kj16609 <bigskyman02@gmail.com>
2023-02-06 06:17:39 -0500

    Fixes logic being wrong for key_map registration

--- src/h95/Importer.cpp
+++ src/h95/Importer.cpp
@@ -3,71 +3,75 @@
 //
 
 #include <spdlog/spdlog.h>
+#include <QCryptographicHash>
+#include <QFile>
 #include "Importer.hpp"
 #include "h95/database/Record.hpp"
+#include "config.hpp"
 
-bool is_subpath( const std::filesystem::path& path, const std::filesystem::path& base )
-{
-	auto rel { std::filesystem::relative( path, base ) };
-	return !rel.empty() && rel.native()[0] != '.';
-}
 
 void Importer::import_game(
 	const QString& title,
 	const QString& creator,
 	const QString& version,
 	const QString& engine,
-	const bool delete_after)
+	const bool delete_after )
 {
 	if ( m_root != m_source )
 	{
 		std::filesystem::create_directories( m_root );
+		spdlog::debug("Copying {} -> {}", m_source.string(), m_root.string());
 		std::filesystem::copy( m_source, m_root );
 
-		std::filesystem::create_directories( m_root / "h95" );
-
-		if(!m_banner.empty())
+		if ( !m_banner.empty() )
 		{
-			const auto rel1 { std::filesystem::relative( m_banner, m_source ) };
+			const auto banner_path_str {getSettings<QString>("paths/h95_banners", "./data/banners")};
+			const std::filesystem::path banner_path {banner_path_str.toStdString()};
+			std::filesystem::create_directories(banner_path);
 
-			spdlog::debug( "{} vs {} -> {}", m_banner.string(), m_source.string(), rel1.string() );
+			auto hasher {QCryptographicHash(QCryptographicHash::Algorithm::Sha256)};
 
-			if ( is_subpath(
-					 m_banner,
-					 m_source ) )  //Banner is inside of root. Copy it from where it is to the new destination
-				std::filesystem::rename( m_root / rel1, m_root / "h95" / m_banner.filename() );
-			else  //Banner is not inside of root
-				std::filesystem::copy( m_banner, m_root / "h95" / m_banner.filename() );
+			QFile file {m_banner};
+			file.open(QFile::OpenModeFlag::ReadOnly);
+			const auto file_data {file.readAll()};
+			hasher.addData(file_data);
+
+			const auto dest_path {banner_path / (hasher.result().toHex(0).toStdString() + m_banner.extension().string())};
+
+			file.copy(dest_path);
 
 			if ( delete_after ) std::filesystem::remove( m_banner );
 
-			m_banner = std::filesystem::relative( m_root / "h95" / m_banner.filename(), m_root );
+			m_banner = dest_path;
 		}
+
+		const auto preview_path_str {getSettings<QString>("paths/h95_previews", "./data/previews")};
+		const std::filesystem::path preview_path {preview_path_str.toStdString()};
+		std::filesystem::create_directories(preview_path);
 
 		//Copy previews
 		for ( auto& preview : m_previews )
 		{
-			const auto rel_preview { std::filesystem::relative( preview, m_root ) };
+			auto hasher { QCryptographicHash( QCryptographicHash::Algorithm::Sha256 ) };
 
-			std::filesystem::create_directories(m_root / "h95" / "previews");
+			QFile file { preview };
+			file.open(QFile::OpenModeFlag::ReadOnly);
+			const auto file_data { file.readAll() };
+			hasher.addData( file_data );
 
-			if ( is_subpath( preview, m_root ) )  //Preview is inside root
-				std::filesystem::rename( preview / rel_preview, m_root / "h95" / "previews" / preview.filename() );
-			else
-				std::filesystem::copy( preview, m_root / "h95" / "previews" / preview.filename() );
+			const auto dest_path {
+				preview_path / ( hasher.result().toHex( 0 ).toStdString() + preview.extension().string() ) };
 
-			if(delete_after)
-				std::filesystem::remove(preview);
+			file.copy( dest_path );
 
-			preview = std::filesystem::relative( m_root / "h95" / "previews" / preview.filename(), m_root );
+			if ( delete_after ) std::filesystem::remove( preview );
+
+			preview = dest_path;
 		}
-
-		if(delete_after)
-			std::filesystem::remove(m_source);
 	}
 
-	m_executable = std::filesystem::relative(m_executable, m_source);
+	m_executable = std::filesystem::relative( m_executable, m_source );
 
 	//Import
-	Record::create( title, creator, version, engine, { m_root, m_executable }, m_banner, m_previews );
+	Record::create( title, creator, engine, { version, m_root, m_executable }, m_banner, m_previews );
 }
