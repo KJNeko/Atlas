70adf01806fa9f722c1015c5255fb8f703a60b32
kj16609 <bigskyman02@gmail.com>
2023-02-11 02:40:53 -0500

    Formatting

--- src/h95/Importer.cpp
+++ src/h95/Importer.cpp
@@ -26,25 +26,26 @@ void Importer::import_game(
 	if ( m_root != m_source )
 	{
 		std::filesystem::create_directories( m_root );
-		spdlog::debug("Copying {} -> {}", m_source.string(), m_root.string());
+		spdlog::debug( "Copying {} -> {}", m_source.string(), m_root.string() );
 		std::filesystem::copy( m_source, m_root, std::filesystem::copy_options::recursive );
 
 		if ( !m_banner.empty() )
 		{
-			const auto banner_path_str {getSettings<QString>("paths/h95_banners", "./data/banners")};
-			const std::filesystem::path banner_path {banner_path_str.toStdString()};
-			std::filesystem::create_directories(banner_path);
+			const auto banner_path_str { getSettings< QString >( "paths/h95_banners", "./data/banners" ) };
+			const std::filesystem::path banner_path { banner_path_str.toStdString() };
+			std::filesystem::create_directories( banner_path );
 
-			auto hasher {QCryptographicHash(QCryptographicHash::Algorithm::Sha256)};
+			auto hasher { QCryptographicHash( QCryptographicHash::Algorithm::Sha256 ) };
 
-			QFile file {m_banner};
-			file.open(QFile::OpenModeFlag::ReadOnly);
-			const auto file_data {file.readAll()};
-			hasher.addData(file_data);
+			QFile file { m_banner };
+			file.open( QFile::OpenModeFlag::ReadOnly );
+			const auto file_data { file.readAll() };
+			hasher.addData( file_data );
 
-			const auto dest_path {banner_path / (hasher.result().toHex(0).toStdString() + m_banner.extension().string())};
+			const auto dest_path {
+				banner_path / ( hasher.result().toHex( 0 ).toStdString() + m_banner.extension().string() ) };
 
-			file.copy(dest_path);
+			file.copy( dest_path );
 
 			if ( delete_after ) std::filesystem::remove( m_banner );
 
@@ -58,9 +59,9 @@ void Importer::import_game(
 			m_banner = dest_path;
 		}
 
-		const auto preview_path_str {getSettings<QString>("paths/h95_previews", "./data/previews")};
-		const std::filesystem::path preview_path {preview_path_str.toStdString()};
-		std::filesystem::create_directories(preview_path);
+		const auto preview_path_str { getSettings< QString >( "paths/h95_previews", "./data/previews" ) };
+		const std::filesystem::path preview_path { preview_path_str.toStdString() };
+		std::filesystem::create_directories( preview_path );
 
 		//Copy previews
 		for ( auto& preview : m_previews )
@@ -68,7 +69,7 @@ void Importer::import_game(
 			auto hasher { QCryptographicHash( QCryptographicHash::Algorithm::Sha256 ) };
 
 			QFile file { preview };
-			file.open(QFile::OpenModeFlag::ReadOnly);
+			file.open( QFile::OpenModeFlag::ReadOnly );
 			const auto file_data { file.readAll() };
 			hasher.addData( file_data );
 
