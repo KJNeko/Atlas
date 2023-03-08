fa49e577a017fdcf4dc4bb27614810599cb62476
kj16609 <bigskyman02@gmail.com>
2023-03-08 14:20:45 -0500

    Update Database to create alternative schema

--- src/h95/database/Database.cpp
+++ src/h95/database/Database.cpp
@@ -39,10 +39,25 @@ void Database::initalize( const std::filesystem::path init_path )
 	Transaction transaction;
 
 	const std::vector< std::string > table_queries {
+
+		"CREATE TABLE records (record_id INTEGER PRIMARY KEY, title TEXT, creator TEXT, engine TEXT, last_played_record DATE, total_playtime INTEGER, UNIQUE(title, creator, engine));",
+		"CREATE TABLE game_metadata (record_id INTEGER REFERENCES records(record_id), version TEXT UNIQUE, game_path TEXT, exec_path TEXT, in_place, last_played DATE, version_playtime INTEGER);",
+		"CREATE TABLE images (record_id INTEGER REFERENCES records(record_id), type INTEGER, sha256 TEXT, UNIQUE(record_id, type, sha256));",
+
+		//Dummy tables. Should be filled out later (Exists to allow X_mapping to use `REFERENCE`
+		"CREATE TABLE f95zone_data (f95_id INTEGER PRIMARY KEY);",
+		"CREATE TABLE dlsite_data (dlsite_id TEXT PRIMARY KEY);",
+
+		"CREATE TABLE f95zone_mapping (record_id REFERENCES records(record_id), f95_id REFERENCES f95zone_data(f95_id), UNIQUE(record_id, f95_id));",
+		"CREATE TABLE dlsite_mapping (record_id REFERENCES records(record_id), dlsite_id REFERENCES dlsite_data(dlsite_id), UNIQUE(record_id, dlsite_id));"
+
+		//Old tables
+		/*
 		"CREATE TABLE IF NOT EXISTS records (record_id INTEGER PRIMARY KEY, title TEXT, creator TEXT, engine TEXT)",
 		"CREATE TABLE IF NOT EXISTS game_metadata (record_id INTEGER REFERENCES records(record_id), version TEXT, game_path TEXT, exec_path TEXT, UNIQUE(record_id, version, game_path, exec_path))",
 		"CREATE TABLE IF NOT EXISTS images (record_id INTEGER REFERENCES records(record_id), type TEXT, path TEXT)",
-		"CREATE TABLE IF NOT EXISTS flags (record_id INTEGER REFERENCES records(record_id) PRIMARY KEY, installed INTEGER, played INTEGER, wanted INTEGER)" };
+		"CREATE TABLE IF NOT EXISTS flags (record_id INTEGER REFERENCES records(record_id) PRIMARY KEY, installed INTEGER, played INTEGER, wanted INTEGER)"*/
+	};
 
 	for ( const auto& query_str : table_queries )
 	{
