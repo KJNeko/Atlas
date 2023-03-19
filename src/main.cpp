ff24cf8a9c08818a789e88e8863f69ac25471df2
towerwatchman <towerwatchman@gmail.com>
2023-03-19 01:15:33 -0400

    Updated .gitignore, Added frame for gui and dialog box. Added resource file for images. Added qss templates. Still need to implement way to load templates.

--- src/main.cpp
+++ src/main.cpp
@@ -1,89 +1,25 @@
-//
-// Created by kj16609 on 1/11/23.
-//
+
+#include "ui/mainwindow.h"
 
 #include <filesystem>
 #include <fstream>
-
 #include <QApplication>
+#include <QFile>
 
-#include "h95/database/Database.hpp"
-#include "h95/config.hpp"
-#include "h95/logging.hpp"
 
-#ifdef TRACY_ENABLE
-#include <tracy/Tracy.hpp>
-void* operator new( std::size_t count )
+int main(int argc, char *argv[])
 {
-	auto ptr = malloc( count );
-	TracySecureAlloc( ptr, count );
-	return ptr;
-}
+    QApplication app(argc, argv);
 
+    //NEED TO ADD A CLASS TO LOAD CUSTOM qss FILES. WE WILL NEED TO MAKE A NEW DIR SO THE CAN BE ADDED.
+    //QFile styleFile( "C:/Users/tower/source/repos/hydrus95_gui/ui/qss/OledBlack.qss" );
+    //styleFile.open( QFile::ReadOnly );
 
-void* operator new[]( std::size_t count )
-{
-	auto ptr = malloc( count );
-	TracySecureAlloc( ptr, count );
-	return ptr;
-}
+    // Apply the loaded stylesheet
+    //QString styleSheet = QLatin1String(styleFile.readAll());
+    //app.setStyleSheet(styleSheet);
 
-
-void operator delete( void* ptr ) noexcept
-{
-	TracySecureFree( ptr );
-	free( ptr );
-}
-
-
-void operator delete[]( void* ptr ) noexcept
-{
-	TracySecureFree( ptr );
-	free( ptr );
-}
-
-
-void operator delete( void* ptr, std::size_t ) noexcept
-{
-	TracySecureFree( ptr );
-	free( ptr );
-}
-
-
-void operator delete[]( void* ptr, std::size_t ) noexcept
-{
-	TracySecureFree( ptr );
-	free( ptr );
-}
-#endif
-
-void spdlog_flush()
-{
-	spdlog::shutdown();
-}
-
-int main( int argc, char** argv )
-{
-	initLogging();
-
-	(void) std::atexit( spdlog_flush );
-	spdlog::flush_on( spdlog::level::err );
-
-	QApplication app { argc, argv };
-
-	const auto path { getSettings< QString >( "paths/data", "./data" ).toStdString() };
-	std::filesystem::create_directories( path );
-
-#ifdef NDEBUG
-	if ( getSettings< bool >( "debug/very_vocal", false ) )
-	{
-		spdlog::set_level( spdlog::level::debug );
-		spdlog::debug( "Debugging enabled." );
-	}
-#else
-	spdlog::set_level(spdlog::level::debug);
-	spdlog::debug("Debugging forcefully enabled due to not compiling with NDEBUG");
-#endif
-
-	Database::initalize("./data/hydrus95.db");
+    MainWindow w;
+    w.show();
+    return app.exec();
 }
