b1c043f732e30a95e5d37cd739f16a7f0edd471f
kj16609 <bigskyman02@gmail.com>
2023-03-31 17:15:34 -0400

    Have the database always try to create the tables

--- src/h95/database/Database.cpp
+++ src/h95/database/Database.cpp
@@ -48,44 +48,39 @@ try
 
 	internal::db = new sqlite::database( init_path.string() );
 
-	if ( config::db::first_start::get() || init_path == ":memory:" )
-	{
-		NonTransaction transaction;
+	NonTransaction transaction;
 
-		const std::vector< std::string > table_queries {
-			"CREATE TABLE records (record_id INTEGER PRIMARY KEY, title TEXT, creator TEXT, engine TEXT, last_played_r DATE, total_playtime INTEGER, UNIQUE(title, creator, engine));",
-			"CREATE TABLE game_metadata (record_id INTEGER REFERENCES records(record_id), version TEXT UNIQUE, game_path TEXT, exec_path TEXT, in_place, last_played DATE, version_playtime INTEGER);",
-			"CREATE TABLE images (record_id INTEGER REFERENCES records(record_id), type INTEGER, path TEXT, UNIQUE(record_id, type, path));",
-			"CREATE TABLE tags (tag_id INTEGER PRIMARY KEY, tag TEXT UNIQUE)",
-			"CREATE TABLE tag_mappings (record_id INTEGER REFERENCES records(record_id), tag_id REFERENCES tags(tag_id), UNIQUE(record_id, tag_id))",
+	const std::vector< std::string > table_queries {
+		"CREATE TABLE IF NOT EXISTS records (record_id INTEGER PRIMARY KEY, title TEXT, creator TEXT, engine TEXT, last_played_r DATE, total_playtime INTEGER, UNIQUE(title, creator, engine));",
+		"CREATE TABLE IF NOT EXISTS game_metadata (record_id INTEGER REFERENCES records(record_id), version TEXT UNIQUE, game_path TEXT, exec_path TEXT, in_place, last_played DATE, version_playtime INTEGER);",
+		"CREATE TABLE IF NOT EXISTS images (record_id INTEGER REFERENCES records(record_id), type INTEGER, path TEXT, UNIQUE(record_id, type, path));",
+		"CREATE TABLE IF NOT EXISTS tags (tag_id INTEGER PRIMARY KEY, tag TEXT UNIQUE)",
+		"CREATE TABLE IF NOT EXISTS tag_mappings (record_id INTEGER REFERENCES records(record_id), tag_id REFERENCES tags(tag_id), UNIQUE(record_id, tag_id))",
 
-			//Dummy tables. Should be filled out later (Exists to allow X_mapping to use `REFERENCE`
-			"CREATE TABLE f95zone_data (f95_id INTEGER PRIMARY KEY);",
-			"CREATE TABLE dlsite_data (dlsite_id TEXT PRIMARY KEY);",
+		//Dummy tables. Should be filled out later (Exists to allow X_mapping to use `REFERENCE`
+		"CREATE TABLE IF NOT EXISTS f95zone_data (f95_id INTEGER PRIMARY KEY);",
+		"CREATE TABLE IF NOT EXISTS dlsite_data (dlsite_id TEXT PRIMARY KEY);",
 
-			"CREATE TABLE f95zone_mapping (record_id REFERENCES records(record_id), f95_id REFERENCES f95zone_data(f95_id), UNIQUE(record_id, f95_id));",
-			"CREATE TABLE dlsite_mapping (record_id REFERENCES records(record_id), dlsite_id REFERENCES dlsite_data(dlsite_id), UNIQUE(record_id, dlsite_id));"
+		"CREATE TABLE IF NOT EXISTS f95zone_mapping (record_id REFERENCES records(record_id), f95_id REFERENCES f95zone_data(f95_id), UNIQUE(record_id, f95_id));",
+		"CREATE TABLE IF NOT EXISTS dlsite_mapping (record_id REFERENCES records(record_id), dlsite_id REFERENCES dlsite_data(dlsite_id), UNIQUE(record_id, dlsite_id));"
 
-			//Old tables
-			/*
+		//Old tables
+		/*
 		"CREATE TABLE IF NOT EXISTS records (record_id INTEGER PRIMARY KEY, title TEXT, creator TEXT, engine TEXT)",
 		"CREATE TABLE IF NOT EXISTS game_metadata (record_id INTEGER REFERENCES records(record_id), version TEXT, game_path TEXT, exec_path TEXT, UNIQUE(record_id, version, game_path, exec_path))",
 		"CREATE TABLE IF NOT EXISTS images (record_id INTEGER REFERENCES records(record_id), type TEXT, path TEXT)",
 		"CREATE TABLE IF NOT EXISTS flags (record_id INTEGER REFERENCES records(record_id) PRIMARY KEY, installed INTEGER, played INTEGER, wanted INTEGER)"*/
-		};
+	};
 
-		for ( const auto& query_str : table_queries )
-		{
-			transaction << query_str;
-			spdlog::debug( "Executing {}", query_str );
-		}
-
-		transaction.commit();
-
-		config::db::first_start::set( false );
+	for ( const auto& query_str : table_queries )
+	{
+		transaction << query_str;
+		spdlog::debug( "Executing {}", query_str );
 	}
-	else
-		spdlog::info( "Database has been initalized before! Skipping table creation" );
+
+	transaction.commit();
+
+	config::db::first_start::set( false );
 }
 catch ( sqlite::sqlite_exception& e )
 {
