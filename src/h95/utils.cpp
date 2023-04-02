907b4801264dcb4c397cbce7557de45b66157a9e
kj16609 <bigskyman02@gmail.com>
2023-04-02 16:13:38 -0400

    Add check for folderSize to check if folder really exists

--- src/h95/utils.cpp
+++ src/h95/utils.cpp
@@ -6,10 +6,16 @@
 
 #include <tracy/Tracy.hpp>
 
+#include "h95/logging.hpp"
+
 std::size_t folderSize( const std::filesystem::path& folder )
 {
 	ZoneScoped;
 	std::size_t counter { 0 };
+
+	spdlog::debug( "Calculating filesize of folder {}", folder );
+
+	if ( !std::filesystem::exists( folder ) ) return 0;
 
 	for ( const auto& file : std::filesystem::recursive_directory_iterator( folder ) )
 		if ( file.is_regular_file() ) counter += std::filesystem::file_size( file );
