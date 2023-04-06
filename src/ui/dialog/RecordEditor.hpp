9c5def49ca74da1cd6d88d820721b9b807963845
kj16609 <bigskyman02@gmail.com>
2023-04-05 23:24:46 -0400

    Initial record editor work

---
+++ src/ui/dialog/RecordEditor.hpp
@@ -0,0 +1,38 @@
+//
+// Created by kj16609 on 4/4/23.
+//
+
+#ifndef HYDRUS95_RECORDEDITOR_HPP
+#define HYDRUS95_RECORDEDITOR_HPP
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+
+namespace Ui
+{
+	class RecordEditor;
+}
+
+QT_END_NAMESPACE
+
+class RecordEditor : public QDialog
+{
+	Q_OBJECT
+	Q_DISABLE_COPY_MOVE(RecordEditor)
+
+  public:
+
+	explicit RecordEditor( QWidget *parent = nullptr );
+	~RecordEditor() override;
+
+  private:
+
+	Ui::RecordEditor *ui;
+
+
+  private slots:
+	void on_
+};
+
+#endif //HYDRUS95_RECORDEDITOR_HPP
