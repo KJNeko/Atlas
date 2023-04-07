2ee95177f8b06adfabb13f675c5d6b0e97057963
kj16609 <bigskyman02@gmail.com>
2023-04-06 21:24:43 -0400

    Some cleanup

--- src/h95/imageManager.cpp
+++ src/h95/imageManager.cpp
@@ -40,30 +40,27 @@ namespace imageManager
 
 	std::filesystem::path importImage( const std::filesystem::path& path )
 	{
-		spdlog::info("Attemping To Load Image");
+		spdlog::info( "Attemping To Load Image" );
 		ZoneScoped;
 		if ( std::filesystem::exists( path ) )
 		{
-			QString qstr = QString::fromStdString( path.string() );
+			const QString qstr { QString::fromStdString( path.string() ) };
 			QImage temp_image;
-			temp_image.load(qstr);
-			if(temp_image.load(qstr) == true){spdlog::info("Image Loaded Sucessfully");}
+			if ( temp_image.load( qstr ) == true )
+			{
+				spdlog::info( "Image Loaded Sucessfully" );
+			}
 
 			//Save the image to a temp file depending on os
 			const std::filesystem::path temp_path { std::filesystem::temp_directory_path() / "h95" / "temp.webp" };
-			spdlog::info("Output Path:{}", temp_path.string());
-			if ( !std::filesystem::exists( temp_path.parent_path() ) )
-			{
-				create_directories( temp_path.parent_path() );
-			}
+			spdlog::info( "Temp Output Path:{}", temp_path.string() );
 
-			if constexpr(sys::is_windows)
-			{
+			if ( !std::filesystem::exists( temp_path.parent_path() ) ) create_directories( temp_path.parent_path() );
+
+			if constexpr ( sys::is_windows )
 				temp_image.save( QString::fromStdString( temp_path.string() ), "png", 100 );
-			}
-			else{
+			else
 				temp_image.save( QString::fromStdString( temp_path.string() ), "webp", 100 );
-			}
 
 			if ( std::ifstream ifs( temp_path ); ifs )
 			{
@@ -79,10 +76,11 @@ namespace imageManager
 					( config::paths::images::getPath() / hash.result().toHex().toStdString() ).string() + ".webp"
 				};
 
-				if ( !std::filesystem::exists( dest.parent_path() ) )
+				if ( !std::filesystem::exists( dest.parent_path() )
+				     && !std::filesystem::create_directories( dest.parent_path() ) )
 				{
-					if ( !std::filesystem::create_directories( dest.parent_path() ) )
-						throw std::runtime_error( fmt::format( "Failed to create directory {}", dest.parent_path() ) );
+					throw std::runtime_error(
+						fmt::format( "importImage: Failed to create directory {}", dest.parent_path() ) );
 				}
 
 				//Copy the temp_file to the destination
@@ -91,7 +89,8 @@ namespace imageManager
 				else
 					return dest;
 
-				if ( !std::filesystem::exists( dest ) ) throw std::runtime_error( "Save failed!" );
+				if ( !std::filesystem::exists( dest ) )
+					throw std::runtime_error( fmt::format( "importImage: Failed to save file to {}", dest ) );
 
 				return dest;
 			}
