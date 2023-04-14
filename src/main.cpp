d777348e56412012175d7d2f5c1c06ff8eae5ec8
kj16609 <bigskyman02@gmail.com>
2023-04-13 20:14:59 -0400

    Adds fix for windeployqt being weird

--- src/main.cpp
+++ src/main.cpp
@@ -43,6 +43,9 @@ int main( int argc, char** argv )
 	std::filesystem::create_directory( config::paths::games::getPath() );
 	std::filesystem::create_directory( config::paths::images::getPath() );
 
+	//Fix for windeployqt not adding the bin directory to itself for some reason
+	QApplication::addLibraryPath( QString::fromStdString( std::filesystem::canonical( "." ).string() ) );
+
 	std::filesystem::path db_path = config::paths::database::getPath() / "atlas.db";
 	Database::initalize( db_path );
 
