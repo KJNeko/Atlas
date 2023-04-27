53897d207673590fa16ba5725c891e0a5f9156ef
kj16609 <bigskyman02@gmail.com>
2023-04-27 12:02:36 -0400

    Fixes operator new missing sized deallocation

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,31 @@ void operator delete( void* ptr ) noexcept
 {
 	TracyFree( ptr );
 	free( ptr );
-	;
+}
+
+void operator delete( void* ptr, std::size_t ) noexcept
+{
+	TracyFree( ptr );
+	free( ptr );
+}
+
+void* operator new[]( std::size_t count )
+{
+	auto ptr = malloc( count );
+	TracyAlloc( ptr, count );
+	return ptr;
+}
+
+void operator delete[]( void* ptr ) noexcept
+{
+	TracyFree( ptr );
+	free( ptr );
+}
+
+void operator delete[]( void* ptr, std::size_t ) noexcept
+{
+	TracyFree( ptr );
+	free( ptr );
 }
 
 #endif
