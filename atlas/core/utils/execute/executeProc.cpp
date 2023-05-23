f2b93664b4ccc844b5e82ab5a4b814643aab3ee6
kj16609 <bigskyman02@gmail.com>
2023-05-23 17:12:33 -0400

    Hopefully fixes windows eating dirt on string conversions

--- atlas/core/utils/execute/executeProc.cpp
+++ atlas/core/utils/execute/executeProc.cpp
@@ -23,9 +23,13 @@ void executeProc( const std::string& path_str )
 {
 	spdlog::debug( "Executing game {}", path_str );
 
-	LPSTR path;
+	LPWSTR path;
 	path = new char[ 4096 * 4 ];
-	std::strcpy( path, path_str.c_str() );
+	//std::strcpy( path, path_str.c_str() );
+	int len { MultiByteToWideChar( CP_ACP, 0, path_str.c_str(), path_str.size() + 1, 0, 0 ) };
+	wchar_t* buff = new wchar_t[ len ];
+	MultiByteToWideChar( CP_ACP, 0, path_str.c_str(), path_str.size() + 1, buff, len );
+	path = buff;
 
 	STARTUPINFOA si;
 	PROCESS_INFORMATION pi;
@@ -34,7 +38,7 @@ void executeProc( const std::string& path_str )
 	si.cb = sizeof( si );
 	ZeroMemory( &pi, sizeof( pi ) );
 
-	if ( !CreateProcessA( nullptr, path, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si, &pi ) )
+	if ( !CreateProcessW( nullptr, path, nullptr, nullptr, FALSE, CREATE_NEW_CONSOLE, nullptr, nullptr, &si, &pi ) )
 		spdlog::error( "Failed to execute process {}", path );
 }
 
