01f904fced98d7e25079f5f121878c62708c2837
kj16609 <bigskyman02@gmail.com>
2023-05-29 19:06:36 -0400

    Fixes file lock being unreliable

--- atlas/main.cpp
+++ atlas/main.cpp
@@ -74,7 +74,7 @@ int main( int argc, char** argv )
 				//Remove the lock file and continue.
 				std::filesystem::remove( "atlas_lock" );
 			}
-		}
+
 #else
 			struct stat sts;
 			const std::string str { "/proc/" + std::to_string( pid ) };
@@ -99,20 +99,9 @@ int main( int argc, char** argv )
 						.arg( QString::number( pid ) ) );
 			}
 #endif
+		}
 	}
-	else
-	{
-		//wtf?
-		QMessageBox::critical(
-			nullptr,
-			"Failed to acquire lock",
-			"Failed to acquire lock! Please make sure you have write permissions to the directory Atlas is in. Report to a dev if this issue persists. Wtf?" );
-	}
-	return EXIT_SUCCESS;
-}
 
-else
-{
 	std::at_quick_exit( clear_lock );
 	std::atexit( clear_lock );
 	if ( std::ofstream ofs( "atlas_lock" ); ofs )
@@ -128,46 +117,45 @@ else
 			"Failed to acquire lock! Please make sure you have write permissions to the directory Atlas is in. Report to a dev if this issue persists." );
 		return EXIT_FAILURE;
 	}
-}
 
-//Fix for windeployqt not adding the bin directory to itself for some reason
-QApplication::addLibraryPath( QString::fromStdString( std::filesystem::canonical( "." ).string() ) );
+	//Fix for windeployqt not adding the bin directory to itself for some reason
+	QApplication::addLibraryPath( QString::fromStdString( std::filesystem::canonical( "." ).string() ) );
 
-qDebug() << QApplication::libraryPaths();
+	qDebug() << QApplication::libraryPaths();
 
-if ( !config::ui::use_system_theme::get() )
-{
-	QFile style_sheet_file { config::paths::theme::get() };
-
-	if ( !style_sheet_file.exists() && !config::ui::use_system_theme::get() )
+	if ( !config::ui::use_system_theme::get() )
 	{
-		config::paths::theme::setDefault();
-		QFile default_sheet { config::paths::theme::get() };
-		default_sheet.open( QFile::ReadOnly );
+		QFile style_sheet_file { config::paths::theme::get() };
 
-		app.setStyleSheet( default_sheet.readAll() );
+		if ( !style_sheet_file.exists() && !config::ui::use_system_theme::get() )
+		{
+			config::paths::theme::setDefault();
+			QFile default_sheet { config::paths::theme::get() };
+			default_sheet.open( QFile::ReadOnly );
+
+			app.setStyleSheet( default_sheet.readAll() );
+		}
+		else
+		{
+			style_sheet_file.open( QFile::ReadOnly );
+
+			app.setStyleSheet( style_sheet_file.readAll() );
+		}
 	}
-	else
-	{
-		style_sheet_file.open( QFile::ReadOnly );
 
-		app.setStyleSheet( style_sheet_file.readAll() );
-	}
-}
+	std::filesystem::create_directory( config::paths::database::getPath().parent_path() );
+	std::filesystem::create_directory( config::paths::games::getPath() );
+	std::filesystem::create_directory( config::paths::images::getPath() );
 
-std::filesystem::create_directory( config::paths::database::getPath().parent_path() );
-std::filesystem::create_directory( config::paths::games::getPath() );
-std::filesystem::create_directory( config::paths::images::getPath() );
+	std::filesystem::path db_path = config::paths::database::getPath() / "atlas.db";
+	Database::initalize( db_path );
 
-std::filesystem::path db_path = config::paths::database::getPath() / "atlas.db";
-Database::initalize( db_path );
+	QPixmapCache::setCacheLimit( 1024 * 128 );
 
-QPixmapCache::setCacheLimit( 1024 * 128 );
-
-MainWindow w;
-w.show();
-const int code { app.exec() };
-Database::deinit();
-clear_lock();
-return code;
+	MainWindow w;
+	w.show();
+	const int code { app.exec() };
+	Database::deinit();
+	clear_lock();
+	return code;
 }
