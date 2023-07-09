e3a32a84ef3b1920c0de7f479ef52adde7ac96ec
towerwatchman <towerwatchman@gmail.com>
2023-07-09 19:08:12 -0400

    added html support. It will load games not sure if they will execute

--- atlas/core/utils/engineDetection/engineDetection.cpp
+++ atlas/core/utils/engineDetection/engineDetection.cpp
@@ -93,7 +93,7 @@ std::vector< std::filesystem::path > detectExecutables( FileScanner& scanner )
 				//potential_executables.emplace_back( relative );
 				continue;
 			}
-			else if ( type.inherits( "text/htm" ) && ext == "index.html" )
+			else if ( type.inherits( "text/plain" ) && filename == "index.html" )
 			{
 				spdlog::info( "Found HTML:{}", path.string() );
 				potential_executables.emplace_back( relative );
