eb1a46daa545e33125511f11d5557d3df2185c35
kj16609 <bigskyman02@gmail.com>
2023-07-20 17:15:46 -0400

    Implement adding/removing user tags from record

--- atlas/core/database/record/Game.cpp
+++ atlas/core/database/record/Game.cpp
@@ -213,14 +213,32 @@ void Game::removePreview( const std::filesystem::path path )
 		removePreview( static_cast< uint64_t >( std::distance( previews.begin(), index ) ) );
 }
 
-void Game::addUserTag( [[maybe_unused]] QString str )
+void Game::addUserTag( QString str )
 {
-	//TODO
+	auto& tags { ptr->m_tags };
+	if ( const auto itter = std::find( tags.begin(), tags.end(), str ); itter == tags.end() )
+	{
+		std::uint64_t tag_id { 0 };
+		RapidTransaction() << "SELECT tag_id FROM tags WHERE tag = ?" << str >> tag_id;
+
+		if ( tag_id == 0 )
+		{
+			RapidTransaction() << "INSERT INTO tags (tag) VALUES (?) RETURNING tag_id" << str >> tag_id;
+		}
+
+		RapidTransaction() << "INSERT INTO tag_mappings (tag_id, record_id) VALUES (?,?)" << tag_id << m_id;
+		tags.emplace_back( str );
+	}
 }
 
-void Game::removeUserTag( [[maybe_unused]] QString str )
+void Game::removeUserTag( QString str )
 {
-	//TODO
+	auto& tags { ptr->m_tags };
+	if ( const auto itter = std::find( tags.begin(), tags.end(), str ); itter != tags.end() )
+	{
+		RapidTransaction() << "DELETE FROM tag_mappings NATURAL JOIN WHERE tag = ? AND record_id = ?" << str << m_id;
+		tags.erase( itter );
+	}
 }
 
 void Game::setBanner( std::filesystem::path path, const BannerType type )
