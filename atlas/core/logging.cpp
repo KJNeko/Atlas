03bb4ae7c6f09616791b5afee8201db73c1e7be4
Cartographic Corsair <CartographicCorsair@gmail.com>
2023-07-29 18:39:37 -0500

    Temporary fix 'ignored warnings' incompatible with Clang.

--- atlas/core/logging.cpp
+++ atlas/core/logging.cpp
@@ -13,8 +13,10 @@
 #pragma GCC diagnostic ignored "-Wswitch-default"
 #pragma GCC diagnostic ignored "-Wsign-conversion"
 #pragma GCC diagnostic ignored "-Wctor-dtor-privacy"
+#ifndef __clang__
 #pragma GCC diagnostic ignored "-Wsuggest-final-types"
 #pragma GCC diagnostic ignored "-Wsuggest-final-methods"
+#endif
 
 #include <fmt/ranges.h>
 #include <spdlog/logger.h>
