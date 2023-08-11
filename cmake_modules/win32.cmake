6e1378c95e483bdd2fbf4d8d81c04b66dbfdc93e
towerwatchman <towerwatchman@gmail.com>
2023-08-11 14:30:50 -0400

    Revert "Updated CMake to add .dll as a suffix and changed where windeployqt is loaded"
    This reverts commit 5215a6d486d7ac7e5c42234d899876b583a98ea2.

--- cmake_modules/win32.cmake
+++ cmake_modules/win32.cmake
@@ -1,15 +1,17 @@
 function(PlatformPreSetup)
 if (WIN32)
-    #set(QT_PATH "C:/Qt/6.4.3/mingw_64")
-    #set(QT_PATH "C:/msys64/clang64")
+    find_program(TOOL_WINDEPLOYQT windeployqt REQUIRED NO_CACHE)
+    message("windeployqt found: ${TOOL_WINDEPLOYQT}")
+    # set(QT_PATH "C:/Qt/6.4.3/mingw_64")
+    set(QT_PATH "C:/msys64/clang64")
+
+    string(APPEND CMAKE_FIND_LIBRARY_SUFFIXES ";.dll")
 
     if (DEFINED ENV{QT_PATH})
         set(QT_PATH $ENV{QT_PATH} PARENT_SCOPE)
         message("Setting QT path from ENV")
-    else()
-        message("Add QT_PATH as Environment variable")
     endif ()
-    
+    set(TOOL_WINDEPLOYQT ${TOOL_WINDEPLOYQT} PARENT_SCOPE)
 endif ()
 endfunction()
 
