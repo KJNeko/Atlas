77fcf20f5d587310a53453f8b5b0c81b3808a2d2
Carto Corsair <105746409+CartographicCorsair@users.noreply.github.com>
2023-08-11 22:40:02 -0500

    Fixing CMake so that WinDeployQt will work (#77)

--- cmake_modules/qt.cmake
+++ cmake_modules/qt.cmake
@@ -1,4 +1,4 @@
 #   /cmake_modules/qt.cmake
 
-set(QT_VERSION  "6.4.3" CACHE STRING PARENT_SCOPE)
-set(QT_PATH "C:/Qt/${QT_VERSION}/mingw_64" CACHE STRING PARENT_SCOPE)
+set(QT_VERSION  "6.4.3" CACHE STRING "Version of Qt being used.")
+set(QT_PATH "C:/Qt/${QT_VERSION}/mingw_64" CACHE PATH "Prefix in which to find Qt.")
--- cmake_modules/win32.cmake
+++ cmake_modules/win32.cmake
@@ -39,7 +39,10 @@ if (WIN32)
             COMMAND ${CMAKE_COMMAND} -E make_directory ${NEEDED_QT_FOLDERS}
                 COMMENT "Creating necessary folders"
         )
-        if(($CMAKE_CXX_PLATFORM_ID} STREQUAL "GNU") OR (${CMAKE_CXX_PLATFORM_ID} STREQUAL "MSVC"))
+        if(
+            (${CMAKE_CXX_PLATFORM_ID} STREQUAL "GNU") 
+            OR (${CMAKE_CXX_PLATFORM_ID} STREQUAL "MINGW") 
+            OR (${CMAKE_CXX_PLATFORM_ID} STREQUAL "MSVC"))
         add_custom_command(TARGET Atlas POST_BUILD
                 COMMAND ${TOOL_WINDEPLOYQT} --compiler-runtime --no-translations --no-system-d3d-compiler --no-opengl-sw
                 $<TARGET_FILE:Atlas>
