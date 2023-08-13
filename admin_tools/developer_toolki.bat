7e8b2971e99cf0e3af7fb7ae6f9269271a0bf795
towerwatchman <towerwatchman@gmail.com>
2023-08-13 17:22:48 -0400

    moved around dirs for scripts

---
+++ admin_tools/developer_toolki.bat
@@ -0,0 +1,3 @@
+powershell.exe -WindowStyle Hidden -file "scripts\ps1\init.ps1"
+
+pause
---
+++ admin_tools/scripts/ps1/init.ps1
@@ -0,0 +1,35 @@
+# .Net methods for GUI
+Add-Type -AssemblyName PresentationFramework
+
+$xamlFile = ($pwd).Path + "\ui\MainWindow.xaml"
+
+#create window
+$inputXML = Get-Content $xamlFile -Raw
+$inputXML = $inputXML -replace 'mc:Ignorable="d"', '' -replace "x:N", 'N' -replace '^<Win.*', '<Window'
+[XML]$XAML = $inputXML
+
+#Read XAML
+$reader = (New-Object System.Xml.XmlNodeReader $xaml)
+try {
+    $window = [Windows.Markup.XamlReader]::Load( $reader )
+}
+catch {
+    Write-Warning $_.Exception
+    throw
+}
+
+# Create variables based on form control names.
+# Variable will be named as 'var_<control name>'
+
+$xaml.SelectNodes("//*[@Name]") | ForEach-Object {
+    #"trying item $($_.Name)"
+    try {
+        Set-Variable -Name "var_$($_.Name)" -Value $window.FindName($_.Name) -ErrorAction Stop
+    }
+    catch {
+        throw
+    }
+}
+Get-Variable var_*
+
+$Null = $window.ShowDialog()
