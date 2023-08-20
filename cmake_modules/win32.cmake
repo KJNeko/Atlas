04d7f4422b2f7dc888309660f95ab697b5f2a1d2
towerwatchman <towerwatchman@gmail.com>
2023-08-20 15:24:06 -0400

    added qt.conf to fix image rescale issues on high DPI settings

--- cmake_modules/win32.cmake
+++ cmake_modules/win32.cmake
@@ -63,6 +63,11 @@ if (WIN32)
                 ${CMAKE_BINARY_DIR}/bin/imageformats/qwebp.dll
                 COMMENT "Copying pre-compiled qwebp.dll for Qt 6.4.3")
 
+        add_custom_command(TARGET Atlas POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/3rdparty/qt.conf
+                ${CMAKE_BINARY_DIR}/bin/qt.conf
+                COMMENT "Copying qt.conf for Window DPI override")
+
     endfunction()   # PlatformPostSetup
 
 endif ()    # if (WIN32)
