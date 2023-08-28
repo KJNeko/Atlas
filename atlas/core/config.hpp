75f4e38b91ba26a59685926e4ff14fc2f89ddd88
kj16609 <bigskyman02@gmail.com>
2023-08-28 13:47:48 -0400

    Bump import thread count up by 1

--- atlas/core/config.hpp
+++ atlas/core/config.hpp
@@ -295,7 +295,7 @@ SETTINGS_D( remote, last_check, int, 0 )
 SETTINGS_D(
 	remote, check_rate, int, std::chrono::duration_cast< std::chrono::seconds >( std::chrono::hours( 24 ) ).count() )
 
-SETTINGS_D( threads, import_threads, int, 1 )
+SETTINGS_D( threads, import_threads, int, 2 )
 SETTINGS_D( threads, image_import_threads, int, 4 )
 SETTINGS_D( threads, image_loader_threads, int, 2 )
 SETTINGS_D( threads, import_pre_loader_threads, int, 4 )
