72763ce10815ce966f366ac25f2ee8aec5ae3b3a
kj16609 <bigskyman02@gmail.com>
2023-09-01 08:25:59 -0400

    Remove extra printouts made during testing

--- atlas/ui/mainwindow.cpp
+++ atlas/ui/mainwindow.cpp
@@ -77,7 +77,7 @@ MainWindow::MainWindow( QWidget* parent ) : QMainWindow( parent ), ui( new Ui::M
 	//Init remote system
 	atlas::initRemoteHandler();
 
-	for ( int i = 0; i < 8; ++i ) atlas::notifications::createMessage( "Welcome to atlas!" );
+	atlas::notifications::createMessage( "Welcome to atlas!" );
 
 	//Make sure mouse tracking is enabled for view
 	ui->recordView->setMouseTracking( true );
