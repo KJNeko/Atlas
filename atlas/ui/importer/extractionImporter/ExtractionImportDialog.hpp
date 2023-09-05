2806fb3369356b9f85104c3fa9be1e1bbd560f10
towerwatchman <towerwatchman@gmail.com>
2023-09-05 19:25:29 -0400

    added extraction dialog

---
+++ atlas/ui/importer/extractionImporter/ExtractionImportDialog.hpp
@@ -0,0 +1,23 @@
+#ifndef EXTRACTIONIMPORTDIALOG_HPP
+#define EXTRACTIONIMPORTDIALOG_HPP
+
+#include <QDialog>
+
+namespace Ui {
+class ExtractionImportDialog;
+}
+
+class ExtractionImportDialog : public QDialog
+{
+    Q_DISABLE_COPY_MOVE( BatchImportDialog )
+    Q_OBJECT
+
+public:
+    explicit ExtractionImportDialog(QWidget *parent = nullptr);
+    ~ExtractionImportDialog();
+
+private:
+    Ui::ExtractionImportDialog *ui;
+};
+
+#endif // EXTRACTIONIMPORTDIALOG_HPP
