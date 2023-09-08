14590f9ac70505e49fccb4a59474a43a52dd06f9
kj16609 <bigskyman02@gmail.com>
2023-09-08 17:09:21 -0400

    Cleanup a lot of the database stuff and move binders and extractors to seperate files for readability. Better handles compilation errors and prints out some of our own to better explain issues

--- atlas/core/database/Binder.hpp
+++ atlas/core/database/Binder.hpp
@@ -2,6 +2,7 @@
 // Created by kj16609 on 6/23/23.
 //
 
+#pragma once
 #ifndef ATLASGAMEMANAGER_BINDER_HPP
 #define ATLASGAMEMANAGER_BINDER_HPP
 
@@ -13,72 +14,9 @@
 
 #include "Database.hpp"
 #include "FunctionDecomp.hpp"
+#include "binders.hpp"
 #include "core/logging/dev.hpp"
-
-template < std::uint64_t index, typename T >
-	requires std::is_integral_v< T >
-void extract( sqlite3_stmt* stmt, T& t ) noexcept
-{
-	t = static_cast< T >( sqlite3_column_int64( stmt, index ) );
-}
-
-template < std::uint64_t index, typename T >
-	requires std::is_same_v< T, QString >
-void extract( sqlite3_stmt* stmt, QString& t ) noexcept
-{
-	const unsigned char* txt { sqlite3_column_text( stmt, index ) };
-
-	if ( txt == nullptr )
-	{
-		t = {};
-		return;
-	}
-	else
-	{
-		t = QString::fromUtf8( txt );
-		return;
-	}
-}
-
-template < std::uint64_t index, typename T >
-	requires std::is_same_v< T, std::filesystem::path >
-void extract( sqlite3_stmt* stmt, std::filesystem::path& t ) noexcept
-{
-	QString str;
-	extract< index, QString >( stmt, str );
-	t = { std::move( str.toStdWString() ) };
-}
-
-template < std::uint64_t index, typename T >
-	requires std::is_same_v< T, std::vector< std::byte > >
-void extract( sqlite3_stmt* stmt, std::vector< std::byte >& t ) noexcept
-{
-	const void* data { sqlite3_column_blob( stmt, index ) };
-	const std::size_t size { static_cast< std::size_t >( sqlite3_column_bytes( stmt, index ) ) };
-
-	t.resize( size );
-	std::memcpy( t.data(), data, size );
-}
-
-template < std::uint64_t index, typename... Args >
-void extractRow( sqlite3_stmt* stmt, std::tuple< Args... >& tpl ) noexcept
-{
-	auto& ref { std::get< index >( tpl ) };
-	extract< index, std::remove_reference_t< decltype( ref ) > >( stmt, ref );
-
-	if constexpr ( index < sizeof...( Args ) - 1 ) extractRow< index + 1, Args... >( stmt, tpl );
-}
-
-template < typename T >
-	requires( !std::is_integral_v< T > )
-int bindParameter( sqlite3_stmt* stmt, const T val, const int idx ) noexcept;
-
-template < typename T >
-	requires std::is_integral_v< T >
-int bindParameter( sqlite3_stmt* stmt, const T val, const int idx ) noexcept
-{
-	return sqlite3_bind_int64( stmt, idx, static_cast< sqlite3_int64 >( val ) );
-}
+#include "extractors.hpp"
 
 class Binder
 {
@@ -137,35 +75,35 @@ class Binder
 				max_param_count,
 				std::string( sqlite3_sql( stmt ) ) ) );
 
-		if ( step_ret == SQLITE_ROW ) [[likely]]
+		switch ( step_ret )
 		{
-			extract< 0, T >( stmt, t );
-			return;
-		}
-		else if ( step_ret == SQLITE_DONE )
-			return;
-		else
-		{
-			switch ( step_ret )
-			{
-				default:
-					[[fallthrough]];
-				case SQLITE_MISUSE:
-					[[fallthrough]];
-				case SQLITE_BUSY:
-					[[fallthrough]];
-				case SQLITE_ERROR:
-					{
-						atlas::logging::dev::error( fmt::format(
-							"DB: Query error: \"{}\", Query: \"{}\"",
-							sqlite3_errmsg( &Database::ref() ),
-							sqlite3_expanded_sql( stmt ) ) );
-						throw std::runtime_error( fmt::format(
-							"DB: Query error: \"{}\", Query: \"{}\"",
-							sqlite3_errmsg( &Database::ref() ),
-							sqlite3_expanded_sql( stmt ) ) );
-					}
-			}
+			case SQLITE_ROW:
+				[[likely]]
+				{
+					extract< 0, T >( stmt, t );
+					return;
+				}
+			case SQLITE_DONE:
+				{
+					return;
+				}
+			default:
+				[[fallthrough]];
+			case SQLITE_MISUSE:
+				[[fallthrough]];
+			case SQLITE_BUSY:
+				[[fallthrough]];
+			case SQLITE_ERROR:
+				{
+					atlas::logging::dev::error( fmt::format(
+						"DB: Query error: \"{}\", Query: \"{}\"",
+						sqlite3_errmsg( &Database::ref() ),
+						sqlite3_expanded_sql( stmt ) ) );
+					throw std::runtime_error( fmt::format(
+						"DB: Query error: \"{}\", Query: \"{}\"",
+						sqlite3_errmsg( &Database::ref() ),
+						sqlite3_expanded_sql( stmt ) ) );
+				}
 		}
 	}
 
@@ -214,18 +152,16 @@ class Binder
 		{
 			if ( stmt == nullptr ) throw std::runtime_error( "stmt was nullptr" );
 
-			const auto step_ret { sqlite3_step( stmt ) };
-
-			if ( step_ret == SQLITE_ROW ) [[likely]]
-			{
-				extractRow< 0 >( stmt, tpl );
-				continue;
-			}
-			else if ( step_ret == SQLITE_DONE )
-				return;
-
 			switch ( sqlite3_step( stmt ) )
 			{
+				case SQLITE_ROW:
+					[[likely]]
+					{
+						extractRow< 0 >( stmt, tpl );
+						continue;
+					}
+				case SQLITE_DONE:
+					return;
 				default:
 					{
 						spdlog::error( "Unhandled error in sqlite!" );
