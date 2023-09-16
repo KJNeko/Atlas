c31842694edc003096c75a71fb542ac059f6c701
kj16609 <bigskyman02@gmail.com>
2023-09-16 07:36:26 -0400

    Add in extra version info to About Atlas

--- atlas/ui/dialog/AboutAtlas.cpp
+++ atlas/ui/dialog/AboutAtlas.cpp
@@ -2,6 +2,31 @@
 
 #include <QDesktopServices>
 #include <QUrl>
+#include <QtVersion>
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+
+#pragma GCC diagnostic ignored "-Weffc++"
+#pragma GCC diagnostic ignored "-Wswitch-default"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wctor-dtor-privacy"
+#ifndef __clang__
+#pragma GCC diagnostic ignored "-Wsuggest-final-types"
+#pragma GCC diagnostic ignored "-Wsuggest-final-methods"
+#endif
+
+#include <spdlog/spdlog.h>
+
+#pragma GCC diagnostic pop
+#else
+
+#include <spdlog/spdlog.h>
+
+#endif
+
+#include <lz4.h>
+#include <sqlite3.h>
 
 #include "core/version.hpp"
 #include "ui_AboutAtlas.h"
@@ -10,6 +35,14 @@ AboutAtlas::AboutAtlas( QWidget* parent ) : QDialog( parent ), ui( new Ui::About
 {
 	ui->setupUi( this );
 	ui->versionLabel->setText( ATLASVERSION_STR );
+
+	ui->lbQt6Ver->setText( QString( "Qt6 Version: %1" ).arg( QT_VERSION_STR ) );
+	ui->lbSqlite3Ver->setText( QString( "SQLite3 Version: %1" ).arg( SQLITE_VERSION ) );
+	ui->lbLZ4Ver->setText( QString( "LZ4: %1" ).arg( LZ4_VERSION_STRING ) );
+	ui->lbSPDLogVer->setText( QString( "spdlog: %1.%2.%3" )
+	                              .arg( SPDLOG_VER_MAJOR )
+	                              .arg( SPDLOG_VER_MINOR )
+	                              .arg( SPDLOG_VER_PATCH ) );
 }
 
 AboutAtlas::~AboutAtlas()
