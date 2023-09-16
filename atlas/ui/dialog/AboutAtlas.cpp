fdc331c8d80520d352381227c58118a727645daa
kj16609 <bigskyman02@gmail.com>
2023-09-16 07:25:35 -0400

    Add extra version label to 'About Atlas'

--- atlas/ui/dialog/AboutAtlas.cpp
+++ atlas/ui/dialog/AboutAtlas.cpp
@@ -3,11 +3,13 @@
 #include <QDesktopServices>
 #include <QUrl>
 
+#include "core/version.hpp"
 #include "ui_AboutAtlas.h"
 
 AboutAtlas::AboutAtlas( QWidget* parent ) : QDialog( parent ), ui( new Ui::AboutAtlas )
 {
 	ui->setupUi( this );
+	ui->versionLabel->setText( ATLASVERSION_STR );
 }
 
 AboutAtlas::~AboutAtlas()
