b9b9b5c332c96df1a70618a8e42f3e202ab1265f
kj16609 <bigskyman02@gmail.com>
2023-09-16 05:05:53 -0400

    Fixes "You fucked something up" message for tool tip

--- atlas/ui/models/RecordListModel.cpp
+++ atlas/ui/models/RecordListModel.cpp
@@ -59,6 +59,8 @@ QVariant RecordListModel::data( const QModelIndex& index, int role ) const
 			return QVariant::fromStdVariant( std::variant<
 											 atlas::records::Game >( m_records.at( static_cast<
 																				   std::size_t >( index.row() ) ) ) );
+		case Qt::ToolTipRole:
+			[[fallthrough]];
 		case Qt::StatusTipRole:
 			{
 				return QString( "Game: %1" ).arg( m_records.at( static_cast< std::size_t >( index.row() ) )->m_title );
