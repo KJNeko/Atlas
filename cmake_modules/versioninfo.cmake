c85cb717025fa3728c08dbedfded95deafe5ab7c
kj16609 <bigskyman02@gmail.com>
2023-09-21 09:06:27 -0400

    Properly find git instead of relying on it being in PATH for the version info gathering.

--- cmake_modules/versioninfo.cmake
+++ cmake_modules/versioninfo.cmake
@@ -1,32 +1,42 @@
 # /cmake_modules/versioninfo.cmake
 
 function(SetVersionInfo)
+
+    find_package(Git)
+    if (NOT Git_FOUND AND NOT DEFINED BYPASS_GIT_REQUIREMENT)
+        message(FATAL_ERROR
+                "HEY YOU! YEAH YOU! READ ME WITH YOUR EYES. Git was not found!
+                DO **NOT** **EXPECT** **SUPPORT** if your sending us log
+                information without filling these in manually or letting cmake find git.
+                Go read the docs to figure out how to do this. Or fix your git install for cmake")
+    endif ()
+
     if (DEFINED ATLAS_GIT_BRANCH)
         message("-- Set git branch string to ${ATLAS_GIT_BRANCH}")
     else ()
         #Get the git branch us currently
-        execute_process(COMMAND git rev-parse --abbrev-ref HEAD WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_BRANCH OUTPUT_STRIP_TRAILING_WHITESPACE)
         message("-- Set git branch string to: ${ATLAS_GIT_BRANCH}")
     endif ()
 
     if (DEFINED ATLAS_GIT_REVISION)
         message("-- Set git revision to: ${ATLAS_GIT_REVISION}")
     else ()
-        execute_process(COMMAND git log -1 --format=%H WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_REVISION OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --format=%H WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_REVISION OUTPUT_STRIP_TRAILING_WHITESPACE)
         message("-- Set git revision to ${ATLAS_GIT_REVISION}")
     endif ()
 
     if (DEFINED ATLAS_GIT_TAG)
         message("-- Git tag set to: ${ATLAS_GIT_TAG}")
     else ()
-        execute_process(COMMAND git describe --tags --abbrev=0 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_TAG OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --abbrev=0 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_TAG OUTPUT_STRIP_TRAILING_WHITESPACE)
         message("-- Git tag set to: ${ATLAS_GIT_TAG}")
     endif ()
 
     if (DEFINED ATLAS_GIT_REVISION_BRIEF)
         message("-- Set git revision to: ${ATLAS_GIT_REVISION_BRIEF}")
     else ()
-        execute_process(COMMAND git log -1 --format=%h WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_REVISION_BRIEF OUTPUT_STRIP_TRAILING_WHITESPACE)
+        execute_process(COMMAND ${GIT_EXECUTABLE} log -1 --format=%h WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} OUTPUT_VARIABLE ATLAS_GIT_REVISION_BRIEF OUTPUT_STRIP_TRAILING_WHITESPACE)
         message("-- Set git revision to ${ATLAS_GIT_REVISION_BRIEF}")
     endif ()
 
