dc50704414910e8ea139dae576671116a2214dad
kj16609 <bigskyman02@gmail.com>
2023-09-24 20:13:10 -0400

    Add caching to thumbnails

--- atlas/core/images/thumbnails.cpp
+++ atlas/core/images/thumbnails.cpp
@@ -7,6 +7,8 @@
 #include "core/exceptions.hpp"
 #include "loader.hpp"
 
+inline static atlas::cache::ImageCache thumb_cache;
+
 namespace atlas::images
 {
 	std::filesystem::path createThumbnail( const std::filesystem::path& image_path )
@@ -15,13 +17,12 @@ namespace atlas::images
 		if ( !std::filesystem::exists( image_path ) )
 			throw AtlasException( format_ns::format( "Invalid path: {} does not exist", image_path ) );
 
-		//Create thumbnail
-		const auto dest { thumbnailPath( image_path ) };
 		QPixmap thumb { loadPixmap( image_path ) };
 
 		thumb =
 			thumb.scaled( config::images::thumbnail_x::get(), config::images::thumbnail_y::get(), Qt::KeepAspectRatio );
 
+		const auto dest { thumbnailPath( image_path ) };
 		if ( !thumb.save( QString::fromStdString( dest.string() ), "webp", 95 ) )
 			throw AtlasException( format_ns::format( "Failed to save thumbnail for {} to {}", image_path, dest ) );
 
@@ -49,16 +50,23 @@ namespace atlas::images
 
 		const auto thumb_path { thumbnailPath( path ) };
 
-		logging::debug( "Thumbnail path should be: {}", thumb_path );
-
-		if ( !std::filesystem::exists( thumb_path ) )
-			(void)createThumbnail( path );
+		if ( auto opt = thumb_cache.find( thumb_path.string() ); opt.has_value )
+			return opt.value();
 		else
-			logging::debug( "Thumbnail already existed" );
+		{
+			logging::debug( "Thumbnail path should be: {}", thumb_path );
 
-		auto image { loadPixmap( thumb_path ) };
+			if ( !std::filesystem::exists( thumb_path ) )
+				(void)createThumbnail( path );
+			else
+				logging::debug( "Thumbnail already existed" );
 
-		return image;
+			auto image { loadPixmap( thumb_path ) };
+
+			thumb_cache.insert( thumb_path.string(), image );
+
+			return image;
+		}
 	}
 
 } // namespace atlas::images
