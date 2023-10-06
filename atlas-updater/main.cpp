cb0a67a8c48fc18d5f718c713aa8c9708b11f93a
kj16609 <bigskyman02@gmail.com>
2023-10-06 13:55:11 -0400

    Prepare cmake for new build process

---
+++ atlas-updater/main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by kj16609 on 10/6/23.
+//
+
+int main( int argc, char** argv )
+{}
---
+++ cmake_modules/dependencies.cmake
@@ -0,0 +1,8 @@
+
+
+
+include(dependencies/tracy)
+include(dependencies/qt)
+include(dependencies/fmt)
+include(dependencies/spdlog)
+include(dependencies/lz4)
