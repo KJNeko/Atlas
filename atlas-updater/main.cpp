4193d12bfeb1dd1adcfb1b1cb174af9af100b221
towerwatchman <towerwatchman@gmail.com>
2023-10-24 21:45:36 -0400

    add eleven zip as dependency

--- atlas-updater/main.cpp
+++ atlas-updater/main.cpp
@@ -4,7 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+	//Check if argc is 0 or 1, If it is a 0 dont show the gui
+    
+	QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
