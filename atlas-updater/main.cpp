e9eadb9c829dc4a4ddcb8f12cf4ff6dae029bc3c
towerwatchman <towerwatchman@gmail.com>
2023-10-29 13:03:38 -0400

    removed qt from updater

--- atlas-updater/main.cpp
+++ atlas-updater/main.cpp
@@ -1,11 +1,98 @@
-#include "mainwindow.hpp"
-#include <QApplication>
+//#include "mainwindow.hpp"
+//#include <QApplication>
+#include <elzip.hpp>
+#include <algorithm>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <windows.h>
+#include <winuser.h>
+#include <vector>
 
-int main(int argc, char *argv[])
+
+int main( int argc, char** argv )
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-    //There are two entry points for this program. The first will just extract the files and move them, the second is a full updater.
+    const std::string update_file = std::string( std::getenv( "APPDATA" ) ) + "\\ATLAS\\update.zip";
+    const std::string update_tmp_dir = std::string( std::getenv( "APPDATA" ) ) + "\\ATLAS\\tmp";
+    const std::string atlas_dir =std::filesystem::current_path().string();
+	bool errors = false;
+    std::vector<std::string> files { "AtlasUpdater.exe", "libstdc++-6.dll", "data", "libgcc_s_seh-1.dll", "libwinpthread-1.dll" };
+
+
+	//check that the update file exist
+	if ( std::filesystem::exists( update_file ) )
+	{
+        //extract update.zip
+        elz::extractZip( update_file, update_tmp_dir );
+
+        for (auto const& dir_entry : std::filesystem::directory_iterator{update_tmp_dir}){
+			std::string atlas_dir_path = atlas_dir + "\\" + dir_entry.path().filename().string();
+			std::filesystem::path dst( atlas_dir_path );
+
+
+            if(!(std::find(files.begin(), files.end(), dir_entry.path().filename().string()) != files.end()))
+            {
+                try{
+                    if(std::filesystem::remove_all(dst))
+                    {
+                        std::cout << "file removed" << "\n";
+                    }
+                    if(!dir_entry.is_directory())
+                    {
+                        std::filesystem::copy( dir_entry.path(),
+                                            dst,
+                                            std::filesystem::copy_options::overwrite_existing );
+                    }
+                    else{
+                        std::filesystem::copy( dir_entry.path(),
+                                            dst,
+                                            std::filesystem::copy_options::overwrite_existing | std::filesystem::copy_options::recursive );
+                    }
+                }
+                catch (const std::exception &e)
+                {
+                    std::cout << e.what() << "\n";       
+                    std::cout << dir_entry.path().filename().string() << "  " << dst.string() << '\n';            
+                }
+            }
+        }
+		//errors = false;
+	}
+    else{
+        std::cout << "error" << '\n';
+		errors = true;
+	}
+
+    //Check if there were errors
+    if( errors == true){
+         int msgboxID = MessageBox(
+            NULL,
+            "There was an error while trying to run the updater. You will need to download the latest update from github",
+            "Atlas Updater",
+            MB_ICONERROR | MB_OK
+        );
+
+        switch (msgboxID)
+        {
+        case IDOK:
+            // TODO: add code
+            break;
+        }
+    }
+    else{
+        int msgboxID = MessageBox(
+            NULL,
+            "Update Complete. Press OK to continue",
+            "Atlas Updater",
+            MB_ICONASTERISK  | MB_OK
+        );
+
+        switch (msgboxID)
+        {
+        case IDOK:
+            // TODO: add code
+            break;
+        }
+    }
+    
 }
