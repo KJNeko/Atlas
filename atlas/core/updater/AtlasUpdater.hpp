a79a7998e574bd186bdf6654bff00af349b2eeb4
towerwatchman <towerwatchman@gmail.com>
2023-12-01 00:11:34 -0500

    Fixed pointer member error in AtlasUpdater.hpp

--- atlas/core/updater/AtlasUpdater.hpp
+++ atlas/core/updater/AtlasUpdater.hpp
@@ -19,16 +19,20 @@ namespace atlas
     //Manage Updates for Atlas
     class AtlasUpdater final :  public QObject
     {
+		//Q_DISABLE_COPY_MOVE( AtlasUpdater )
         Q_OBJECT
 
         //QThread m_thread {};
 		QNetworkAccessManager m_manager {};
 
         public:
+		AtlasUpdater();
+		AtlasUpdater(const AtlasUpdater&) = delete;
+		AtlasUpdater& operator=(const AtlasUpdater&) = delete;
+		~AtlasUpdater();
         //AtlasUpdater(QObject* parent, std::shared_ptr<gui_settings> gui_settings);
         void check_for_updates(bool isManual);
-		AtlasUpdater();
-	    ~AtlasUpdater();
+
 
         private:
 
