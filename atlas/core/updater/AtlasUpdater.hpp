dea5fbba7b22e3486b0462578a79e8d29a306961
kj16609 <bigskyman02@gmail.com>
2023-12-01 07:25:17 -0500

    Fix formatting for Updater header

--- atlas/core/updater/AtlasUpdater.hpp
+++ atlas/core/updater/AtlasUpdater.hpp
@@ -1,54 +1,53 @@
 
 
-
-
 #ifndef ATLASGAMEMANAGER_ATLASUPDATER_HPP
 #define ATLASGAMEMANAGER_ATLASUPDATER_HPP
 
 #include <QNetworkReply>
-#include <QThread>
 #include <QProgressDialog>
+#include <QThread>
 
 class QNetworkReply;
 
-#include <filesystem>
 #include <ui/dialog/UpdateDialog.h>
+
+#include <filesystem>
 
 namespace atlas
 {
-    //Manage Updates for Atlas
-    class AtlasUpdater final :  public QObject
-    {
+	//Manage Updates for Atlas
+	class AtlasUpdater final : public QObject
+	{
 		//Q_DISABLE_COPY_MOVE( AtlasUpdater )
-        Q_OBJECT
+		Q_OBJECT
 
-        //QThread m_thread {};
+		//QThread m_thread {};
 		QNetworkAccessManager m_manager {};
 
-        public:
+	  public:
+
 		AtlasUpdater();
-		AtlasUpdater(const AtlasUpdater&) = delete;
-		AtlasUpdater& operator=(const AtlasUpdater&) = delete;
+		AtlasUpdater( const AtlasUpdater& ) = delete;
+		AtlasUpdater& operator=( const AtlasUpdater& ) = delete;
 		~AtlasUpdater();
-        //AtlasUpdater(QObject* parent, std::shared_ptr<gui_settings> gui_settings);
-        void check_for_updates(bool isManual);
+		//AtlasUpdater(QObject* parent, std::shared_ptr<gui_settings> gui_settings);
+		void check_for_updates( bool isManual );
 
-
-        private:
+	  private:
 
 		std::uint64_t converToEpoch( QString time );
 		std::uint64_t converToShortEpoch( QString time );
 		void handleJsonResponse( QNetworkReply* reply, bool isManual );
-		  void handleManifestError( QNetworkReply::NetworkError error, QNetworkReply* reply );
-		  void downloadUpdate( QString url );
-		  void saveFile( QNetworkReply* reply );
-		  int updateMessageBox( QString message, QString windowTitle, bool includeChangelog );
-		  void downloadProgress( int ist, int max );
-		  UpdateDialog* ud = new UpdateDialog();
+		void handleManifestError( QNetworkReply::NetworkError error, QNetworkReply* reply );
+		void downloadUpdate( QString url );
+		void saveFile( QNetworkReply* reply );
+		int updateMessageBox( QString message, QString windowTitle, bool includeChangelog );
+		void downloadProgress( int ist, int max );
+		UpdateDialog* ud = new UpdateDialog();
 	};
-    
-    void initUpdateHandler(bool isManual);
+
+	void initUpdateHandler( bool isManual );
 	void shutdownUpdateHandler();
-}
+} // namespace atlas
 
 #endif //ATLASGAMEMANAGER_ATLASUPDATER_HPP
