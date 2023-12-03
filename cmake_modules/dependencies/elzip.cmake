8457fdbc46b08c9429621634bf76cecc083c9dd1
towerwatchman <towerwatchman@gmail.com>
2023-12-02 20:39:52 -0500

    why cant all comments setters be the same......

--- cmake_modules/dependencies/elzip.cmake
+++ cmake_modules/dependencies/elzip.cmake
@@ -1,5 +1,5 @@
 if (WIN32)
-	//option(MZ_ZLIB "Enables ZLIB compression" OFF)
+	#option(MZ_ZLIB "Enables ZLIB compression" OFF)
 	option(MZ_FORCE_FETCH_LIBS "Enables fetching third-party libraries always" ON)
 	add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/elzip) # Path to the 11zip
 endif ()
