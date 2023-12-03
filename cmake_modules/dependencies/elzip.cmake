f7c1b6f88ceb0132bdc1f6ef5e9e82049fb7f760
towerwatchman <towerwatchman@gmail.com>
2023-12-02 20:37:46 -0500

    force build all dependencies

--- cmake_modules/dependencies/elzip.cmake
+++ cmake_modules/dependencies/elzip.cmake
@@ -1,5 +1,5 @@
 if (WIN32)
-	option(BUILD_SHARED_LIBS "Build shared library" ON) 
-	option(MZ_ZLIB "Enables ZLIB compression" OFF)
+	//option(MZ_ZLIB "Enables ZLIB compression" OFF)
+	option(MZ_FORCE_FETCH_LIBS "Enables fetching third-party libraries always" ON)
 	add_subdirectory(${CMAKE_SOURCE_DIR}/dependencies/elzip) # Path to the 11zip
 endif ()
