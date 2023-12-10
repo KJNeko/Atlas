1bed76c733da4d59ee3b957b8d711b2bba2504e4
towerwatchman <towerwatchman@gmail.com>
2023-12-10 09:11:03 -0500

    main.cpp cleanup

--- atlas-updater/main.cpp
+++ atlas-updater/main.cpp
@@ -15,8 +15,6 @@
 
 int main( int argc, char** argv )
 {
-	//std::cout << argc << '\n';
-
 	if ( argc <= 1 )
 	{
 		int msgboxID = MessageBox(
@@ -32,6 +30,7 @@ int main( int argc, char** argv )
 				exit( 0 );
 		}
 	}
+	else
 	{
 		const std::string update_file = std::string( std::getenv( "APPDATA" ) ) + "\\ATLAS\\update.zip";
 		const std::string update_tmp_dir = std::string( std::getenv( "APPDATA" ) ) + "\\ATLAS\\tmp";
@@ -45,46 +44,6 @@ int main( int argc, char** argv )
 		{
 			//extract update.zip
 			elz::extractZip( update_file, update_tmp_dir );
-
-			/*for (auto const& dir_entry : std::filesystem::directory_iterator{update_tmp_dir}){
-                std::string atlas_dir_path = atlas_dir + "\\" + dir_entry.path().filename().string();
-                std::filesystem::path dst( atlas_dir_path );
-
-
-                if(!(std::find(folders.begin(), folders.end(), dir_entry.path().filename().string()) != folders.end()))
-                {
-                    //Check for updater file
-                    
-                    if(dir_entry.path().filename() != "AtlasUpdater.exe") 
-                    {
-                        std::cout << dir_entry.path().string() << " -> " << dst.string() << '\n';  
-                        try{
-                            if(std::filesystem::remove_all(dst))
-                            {
-                                std::cout << "file removed" << "\n";
-                            }
-                            if(!dir_entry.is_directory())
-                            {
-                                std::filesystem::copy( dir_entry.path(),
-                                                    dst,
-                                                    std::filesystem::copy_options::overwrite_existing );
-                            }
-                            else{
-                                std::filesystem::copy( dir_entry.path(),
-                                                    dst,
-                                                    std::filesystem::copy_options::overwrite_existing | std::filesystem::copy_options::recursive );
-                                std::cout << "file Added" << "\n";
-                            }
-                        }
-                        catch (const std::exception &e)
-                        {
-                            std::cout << e.what() << "\n";       
-                            std::cout << dir_entry.path().filename().string() << "  " << dst.string() << '\n';            
-                        }
-                    }
-                }
-            }*/
-			//errors = false;
 		}
 		else
 		{
@@ -111,13 +70,6 @@ int main( int argc, char** argv )
 		}
 		else
 		{
-			/*int msgboxID = MessageBox(
-                NULL,
-                "Update Complete. Press OK to continue",
-                "Atlas Updater v1.0",
-                MB_ICONASTERISK  | MB_OK
-            );*/
-
 			const std::string ps1_exe = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe";
 			STARTUPINFOA si;
 			PROCESS_INFORMATION pi;
@@ -125,13 +77,8 @@ int main( int argc, char** argv )
 			si.cb = sizeof( si );
 			ZeroMemory( &pi, sizeof( pi ) );
 
-			//Wide string because windows is stupid
-
 			const std::string args = "-noexit Copy-item \'" + update_tmp_dir + "\\*\' -Destination \'" + atlas_dir
 			                       + "\' -Recurse -force ; start \'" + atlas_exe + "\'";
-
-			//std::cout << args << '\n';
-			//system("pause");
 
 			char* win_buffer { new char[ args.size() ] };
 			std::memcpy( win_buffer, args.c_str(), args.size() );
@@ -144,13 +91,6 @@ int main( int argc, char** argv )
 				CloseHandle( pi.hProcess );
 				CloseHandle( pi.hThread );
 			}
-
-			/*switch (msgboxID)
-            {
-            case IDOK:
-                // TODO: add code
-                break;
-            }*/
 		}
 	}
 }
