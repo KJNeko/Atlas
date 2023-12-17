0f96e378482bf2f0285a61309f939990864d8977
towerwatchman <towerwatchman@gmail.com>
2023-12-17 13:22:45 -0500

    Update SettingsDialog.hpp

--- atlas/ui/dialog/SettingsDialog.hpp
+++ atlas/ui/dialog/SettingsDialog.hpp
@@ -80,32 +80,96 @@ class SettingsDialog final : public QDialog
 	void on_sbCapsuleSpace_valueChanged( int num );
 	void on_cbCenterItems_stateChanged( int state );
 	//Overlay Details
-	//overlay
+	//Overlay
 	void on_pbOverlayColor_pressed();
-	//title
-	void on_cbTitleEnable_stateChanged( int state );
-	void on_sp_xtitle_valueChanged( int num );
-	void on_sp_ytitle_valueChanged( int num );
-	void on_sp_title_fontsize_valueChanged( int num );
-	void on_pbTitleBColor_pressed();
-	//engine
-	void on_cbEngineEnable_stateChanged( int state );
-	void on_sp_xengine_valueChanged( int num );
-	void on_sp_yengine_valueChanged( int num );
-	void on_cbEnableEngineColors_stateChanged( int state );
-	//Version
-	void on_cbversionEnable_stateChanged( int state );
-	void on_sp_xversion_valueChanged( int num );
-	void on_sp_yversion_valueChanged( int num );
-	void on_pbVersionBColor_pressed();
-
 	void on_cbOverlayLayout_currentIndexChanged( int index );
-
 	void on_cbFont_currentTextChanged( const QString& text );
 	void on_sbFontSize_valueChanged( int num );
-
 	void on_cbAppFont_currentIndexChanged( [[maybe_unused]] int idx );
 	void on_sbAppFontSize_valueChanged( [[maybe_unused]] int num );
+	//Title
+	void on_cb_title_enable_stateChanged( int state );
+	void on_sp_title_x_valueChanged( int num );
+	void on_sp_title_y_valueChanged( int num );
+	void on_cb_title_align_stateChanged( int state );
+	void on_pb_title_bcolor_pressed();
+	void on_pb_title_fcolor_pressed();
+	void on_sp_title_fontsize_valueChanged( int num );
+	void on_pb_title_bold_pressed();
+	void on_pb_title_italics_pressed();
+	//Engine
+	void on_cb_engine_enable_stateChanged( int state );
+	void on_sp_engine_x_valueChanged( int num );
+	void on_sp_engine_y_valueChanged( int num );
+	void on_cb_engine_align_stateChanged( int state );
+	void on_pb_engine_bcolor_pressed();
+	void on_pb_engine_fcolor_pressed();
+	void on_cb_engine_default_colors_stateChanged( int state );
+	void on_pb_engine_bold_pressed();
+	void on_pb_engine_italics_pressed();
+	//Version
+	void on_cb_version_enable_stateChanged( int state );
+	void on_sp_version_x_valueChanged( int num );
+	void on_sp_version_y_valueChanged( int num );
+	void on_cb_version_align_stateChanged( int state );
+	void on_pb_version_bcolor_pressed();
+	void on_pb_version_fcolor_pressed();
+	void on_pb_version_bold_pressed();
+	void on_pb_version_italics_pressed();
+	//Creator
+	void on_cb_creator_enable_stateChanged( int state );
+	void on_sp_creator_x_valueChanged( int num );
+	void on_sp_creator_y_valueChanged( int num );
+	void on_cb_creator_align_stateChanged( int state );
+	void on_pb_creator_bcolor_pressed();
+	void on_pb_creator_fcolor_pressed();
+	void on_pb_creator_bold_pressed();
+	void on_pb_creator_italics_pressed();
+	//Status
+	void on_cb_status_enable_stateChanged( int state );
+	void on_sp_status_x_valueChanged( int num );
+	void on_sp_status_y_valueChanged( int num );
+	void on_cb_status_align_stateChanged( int state );
+	void on_cb_status_link_stateChanged( int state );
+	void on_pb_status_bold_pressed();
+	void on_pb_status_italics_pressed();
+	//GameType
+	void on_cb_gametype_enable_stateChanged( int state );
+	void on_sp_gametype_x_valueChanged( int num );
+	void on_sp_gametype_y_valueChanged( int num );
+	void on_cb_gametype_align_stateChanged( int state );
+	void on_cb_gametype_link_stateChanged( int state );
+	void on_pb_gametype_bold_pressed();
+	void on_pb_gametype_italics_pressed();
+	//Rating
+	void on_cb_rating_enable_stateChanged( int state );
+	void on_sp_rating_x_valueChanged( int num );
+	void on_sp_rating_y_valueChanged( int num );
+	void on_cb_rating_align_stateChanged( int state );
+	void on_cb_rating_link_stateChanged( int state );
+	//Views
+	void on_cb_views_enable_stateChanged( int state );
+	void on_sp_views_x_valueChanged( int num );
+	void on_sp_views_y_valueChanged( int num );
+	void on_cb_views_align_stateChanged( int state );
+	void on_cb_views_link_stateChanged( int state );
+	//Likes
+	void on_cb_likes_enable_stateChanged( int state );
+	void on_sp_likes_x_valueChanged( int num );
+	void on_sp_likes_y_valueChanged( int num );
+	void on_cb_likes_align_stateChanged( int state );
+	void on_cb_likes_link_stateChanged( int state );
+	//Favorite
+	void on_cb_favorite_enable_stateChanged( int state );
+	void on_sp_favorite_x_valueChanged( int num );
+	void on_sp_favorite_y_valueChanged( int num );
+	void on_cb_favorite_align_stateChanged( int state );
+	void on_cb_favorite_link_stateChanged( int state );
+	//UpdateIcon
+	void on_cb_updateicon_enable_stateChanged( int state );
+	void on_sp_updateicon_x_valueChanged( int num );
+	void on_sp_updateicon_y_valueChanged( int num );
+	void on_cb_updateicon_align_stateChanged( int state );
 };
 
 #endif //ATLAS_SETTINGSDIALOG_HPP
