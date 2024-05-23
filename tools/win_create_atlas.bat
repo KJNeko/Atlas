99c5f1fb39f0e20d50f71c0f3fdb8a4a3ae41085
towerwatchman <towerwatchman@gmail.com>
2024-05-23 15:24:27 -0400

    add missing data to bat file for creating exe

--- tools/win_create_atlas.bat
+++ tools/win_create_atlas.bat
@@ -2,7 +2,7 @@
 
 ECHO Building Atlas. Please wait.....
 
-pyinstaller  --onedir --windowed --add-data "../atlas/assets/themes;./Resources/themes" --icon="../atlas/assets/icons/appicon.ico" --name="Atlas" "../app.py" --noconfirm --distpath="dist"
+pyinstaller  --onedir --windowed --add-data "../atlas/assets/themes;./Resources/themes" --add-data "../atlas/assets/engines/types;./Resources/engines/types" --icon="../atlas/assets/icons/appicon.ico" --name="Atlas" "../app.py" --noconfirm --distpath="dist"
 
 ECHO Building Atlas Debug Console Please wait.....
 pyinstaller --onefile --icon="../atlas/assets/icons/appicon.ico" --name="Atlas Debug Console" "../atlas/debug_console.py" --noconfirm --distpath="dist/Atlas"
