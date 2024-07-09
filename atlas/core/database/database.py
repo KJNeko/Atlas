cbd907b493d54377e0a4009770a62e2a7d38a38e
towerwatchman <towerwatchman@gmail.com>
2024-07-09 18:39:35 -0400

    fix so dummy record is added

--- atlas/core/database/database.py
+++ atlas/core/database/database.py
@@ -23,8 +23,8 @@ class database(object):
         test_creator = "Atlas Games"
         test_engine = "Unity"
 
-        if not GameData.record_exist(test_name, test_creator, test_engine):
-            record_id = version.add_version("Chapter: 1""C:/Atlas Games/Galaxy Crossing First Conquest","Galaxy Crossing First Conquest.exe",0,True )
+        if not GameData.record_exists(test_name, test_creator, test_engine):
+            record_id = version.add_version("Chapter: 1","C:/Atlas Games/Galaxy Crossing First Conquest","Galaxy Crossing First Conquest.exe",0,True )
             banner_image = QImage(":/images/assets/Grid_Capsule_Default.webp")
             #if not banner_image.isNull():      
             #    game.set_banner(record_id,  "./data/images/config_image.webp", banner_type.Normal ) 
